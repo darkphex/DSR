3bf9f774662a26cd928d623db526770b7a58aae2
Phex <crow@mail.de>
2023-05-23 10:13:19 +0200

    refactor: Updated Main.java with a container ship call

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,13 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Containerschiff schiff = new Containerschiff("Berta", 2500, 1500);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        schiff.beladen(10, true); // Belade 10 Container mit 20 TEU
+        schiff.beladen(5, false); // Belade 5 Container mit 40 TEU
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println();
+        System.out.println();
         }
     }
 }
